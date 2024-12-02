37834253030f19e93225be6fb8721bd61dc349ab
emoreira <theeduardogi682@gmail.com>
2024-12-01 21:59:26 -0300

    random scatter

--- vec3.h
+++ vec3.h
@@ -96,4 +96,28 @@ inline vec3 unit_vector(const vec3& v) {
     return v / v.length();
 }
 
+static vec3 random_vec() {
+    return vec3(random_double(), random_double(), random_double());
+}
+
+static vec3 random_vec(double min, double max) {
+    return vec3(random_double(min,max), random_double(min,max), random_double(min,max));
+}
+
+inline vec3 random_unit_vector() {
+    while (true) {
+        auto p = random_vec(-1,1);
+        auto lensq = p.length_squared();
+        if (1e-160 < lensq && lensq <= 1)
+            return p / sqrt(lensq);
+    }
+}
+
+inline vec3 random_on_hemisphere(const vec3& normal) {
+    vec3 on_unit_sphere = random_unit_vector();
+    if (dot(on_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
+        return on_unit_sphere;
+    return -on_unit_sphere;
+}
+
 #endif
